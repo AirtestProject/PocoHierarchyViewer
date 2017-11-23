673790aafff2c62dbb00192201ec19a61ec109bb
lxn3032 <lxn3032@corp.netease.com>
2017-11-23 11:38:29 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ rm PocoHierarchyViewer-win32-x64.zip
 
 mv build--win32-x64/build-.exe build--win32-x64/start.exe 
 ./tools/zip/zip.exe PocoHierarchyViewer-win32-x64.zip -r build--win32-x64/ 
+
+curl -F file=@PocoHierarchyViewer-win32-x64.zip http://192.168.40.218:23456/downloads/poco
