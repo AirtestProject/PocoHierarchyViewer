4f1933b8592431dfe758d8469ac7fa13a11a7ecd
lxn3032 <lxn3032@corp.netease.com>
2017-12-08 15:34:11 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ rm PocoHierarchyViewer-win32-x64.zip
 ./node_modules/gulp/bin/gulp.js build
 ./node_modules/electron-packager/cli.js . --out --overwrite build/
 
-mv build--win32-x64/build-.exe build--win32-x64/start.exe 
+mv build--win32-x64/build-.exe build--win32-x64/start.exe
+mkdir build--win32-x64/lib
+cp -r lib/apk build--win32-x64/lib/
 ./tools/zip/zip.exe PocoHierarchyViewer-win32-x64.zip -r build--win32-x64/ 
 
 curl -F file=@PocoHierarchyViewer-win32-x64.zip http://192.168.40.218:23456/downloads/poco
