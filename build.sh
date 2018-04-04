b6894ba1fd5c23a1408171685caa056aadb3b869
lxn3032 <lxn3032@corp.netease.com>
2018-04-04 18:21:03 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -1,40 +1,37 @@
 sysOS=`uname -s`
-outputDir="build--win32-x64/"
+outputDir="PocoHierarchyViewer-build--win32-x64/"
 zipFile="PocoHierarchyViewer-win32-x64.zip"
 if [ $sysOS == "Darwin" ];then
-	outputDir="build--darwin-x64/"
+    outputDir="PocoHierarchyViewer-build--darwin-x64/"
     zipFile="PocoHierarchyViewer-darwin-x64.zip"
 fi
 
-rm -rf $outputDir 
+rm -rf $outputDir
 rm $zipFile
 ./node_modules/gulp/bin/gulp.js build
-./node_modules/electron-packager/cli.js . --out --overwrite build/
-
+./node_modules/electron-packager/cli.js . --out --overwrite PocoHierarchyViewer-build/
 
 if [ $sysOS == "Darwin" ];then
-	# make venv
-	VENVDIR="venv-darwin"
-	rm -rf $VENVDIR
-	virtualenv $VENVDIR
-	virtualenv --relocatable $VENVDIR
-	$VENVDIR/bin/python -m pip install pocoui
+    # make venv
+    VENVDIR="venv-darwin"
+    rm -rf $VENVDIR
+    virtualenv $VENVDIR
+    virtualenv --relocatable $VENVDIR
+    $VENVDIR/bin/python -m pip install pocoui
 
-	# archive
-	mv $outputDir "PocoHierarchyViewer-$outputDir"
+    # archive
     zip $zipFile -r $outputDir
 else
-	# make venv
-	VENVDIR="venv-win32"
-	rm -rf $VENVDIR
-	virtualenv --always-copy $VENVDIR
-	virtualenv --relocatable $VENVDIR
-	mv $VENVDIR/Scripts $VENVDIR/bin  # 这个文件夹名字有点怪
-	$VENVDIR/bin/python -m pip install pywin32 pocoui
+    # make venv
+    VENVDIR="venv-win32"
+    rm -rf $VENVDIR
+    virtualenv --always-copy $VENVDIR
+    virtualenv --relocatable $VENVDIR
+    mv $VENVDIR/Scripts $VENVDIR/bin  # 这个文件夹名字有点怪
+    $VENVDIR/bin/python -m pip install pywin32 pocoui
 
-	# archive
+    # archive
     mv $outputDir/build-.exe $outputDir/start.exe
-    mv $outputDir "PocoHierarchyViewer-$outputDir"
     ./tools/zip/zip.exe $zipFile -r $outputDir
 fi
 
