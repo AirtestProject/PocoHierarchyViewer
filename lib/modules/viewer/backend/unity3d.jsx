189a681a2d45bf816515ec2a33163eba1c336aaf
祝啸风 <zxfn4514@corp.netease.com>
2018-05-09 22:04:13 +0800

    修改前端使用jsrpc

--- lib/modules/viewer/backend/unity3d.jsx
+++ lib/modules/viewer/backend/unity3d.jsx
@@ -8,6 +8,11 @@ import {Icon} from '../../util/icon'
 
 import {InspectorViewBase} from '../hierarchyViewer'
 
+const RpcClient =  require('../../jsrpc/rpcclient')
+const clientSocket = require('../../jsrpc/transport')
+const Screen = require('../../poco-driver/std/screen')
+const Dumper = require('../../poco-driver/std/dumper')
+
 const Promise = window.require('bluebird')
 const {spawn} = window.require('child_process')
 
@@ -34,139 +39,34 @@ export class Unity3dInspectorView extends InspectorViewBase {
         this.state.sdkVersionCode = 'unknown'
         autoBind(this)
 
-        this.inBox = ''
-
-        let python_executable = `${AppResourcePath}/venv-${window.process.platform}/bin/python`
-        console.log(python_executable)
-        this.pocoProc = spawn(python_executable, ['-u', '-m', 'poco.drivers.unity3d.repl'])
-        this.pocoProc.stdout.on('data', data => {
-            data = data.toString()
-            this.inBox += data
-
-            // parse hierarchy
-            let hierarchyEndIndex = this.inBox.indexOf(HierarchyBoundaryEnd)
-            if (hierarchyEndIndex >= 0) {
-                let hierarchyStartIndex = this.inBox.indexOf(HierarchyBoundary)
-                let jsonHierarchy = this.inBox.substring(hierarchyStartIndex + HierarchyBoundary.length, hierarchyEndIndex)
-                this.setState({hierarchyTree: JSON.parse(jsonHierarchy)})
-                this.inBox = this.inBox.substring(hierarchyEndIndex + HierarchyBoundaryEnd.length)
-            }
-
-            // parse screen
-            let screenEndIndex = this.inBox.indexOf(ScreenBoundaryEnd)
-            if (screenEndIndex >= 0) {
-                let screenStartIndex = this.inBox.indexOf(ScreenBoundary)
-                let screenData = this.inBox.substring(screenStartIndex + ScreenBoundary.length, screenEndIndex)
-                this.setState({screen: screenData})
-                this.inBox = this.inBox.substring(screenEndIndex + ScreenBoundaryEnd.length)
-            }
-
-            // parse profile data
-            let profileDataEndIndex = this.inBox.indexOf(ProfileDataBoundaryEnd)
-            if (profileDataEndIndex >= 0) {
-                let profileDataStartIndex = this.inBox.indexOf(ProfileDataBoundary)
-                let jsonProfileData = this.inBox.substring(profileDataStartIndex + ProfileDataBoundary.length, profileDataEndIndex)
-                let pfData = JSON.parse(jsonProfileData)
-                let {profileData} = this.state
-                Object.assign(profileData, pfData)
-                this.setState({profileData})
-                this.inBox = this.inBox.substring(profileDataEndIndex + ProfileDataBoundaryEnd.length)
-            }
-
-            // parse sdk version code
-            let sdkVersionCodeEndIndex = this.inBox.indexOf(SDKVersionBoundaryEnd)
-            if (sdkVersionCodeEndIndex >= 0) {
-                console.log(this.inBox)
-                let sdkVersionCodeStartIndex = this.inBox.indexOf(SDKVersionBoundary)
-                let versionCodeStr = this.inBox.substring(sdkVersionCodeStartIndex + SDKVersionBoundary.length, sdkVersionCodeEndIndex)
-                this.setState({sdkVersionCode: versionCodeStr})
-                this.inBox = this.inBox.substring(sdkVersionCodeEndIndex + SDKVersionBoundaryEnd.length)
-            }
+        let client_s = new clientSocket.ClientSocket()
+        let client = new RpcClient(client_s)
+        this.connection = client.connect(this.props.ip, this.props.port).then(() => {
+            this.screen = new Screen(client)
+            this.dumper = new Dumper(client)
+            this.refresh()
+            // this.getSDKVersion()
         })
-        this.pocoProc.stderr.on('data', data => {
-            data = data.toString()
-            console.error(data)
-            toastr["warning"](data)
-        })
-        this.pocoProc.on('close', exitCode => {
-        })
-
-        this.refresh(720)
-        this.getSDKVersion()
     }
 
-    execPy(code) {
-        this.pocoProc.stdin.write(code)
-        this.pocoProc.stdin.write('\n')
+    refreshScreen(width) {
+        this.screen.getScreen(width).then(res => {
+            var screenData = "data:image/" + res[1] + ";base64," + res[0]
+            this.setState({screen: screenData})
+        })
     }
 
-    connectAirtestDevice(devUri) {
-        let code = `
-from airtest.core.api import connect_device
-connect_device('${devUri}')
-`  
-        this.execPy(code)
+    refreshDumper() {
+        this.dumper.dumpHierarchy().then(jsonHierarchy => {
+            this.setState({hierarchyTree: jsonHierarchy})
+        })
     }
 
     refresh(width) {
-        let isUnityEditor = this.props.platform === 'windows' && this.props.options.isUnityEditor
-        // 只有windows版的非editor mode 才需要主动connect到device
-        if (this.props.platform === 'windows' && !this.props.options.isUnityEditor) {
-            this.connectAirtestDevice(`Windows:///?title_re=${this.props.options.titleRe}&class_name=UnityWndClass`)
-        }
-
-        let code = `
-def get_hierarchy_and_screen():
-    # cache poco instance globally to speed up
-    poco = globals().get('poco')
-    if poco is None:
-        poco = UnityPoco(("${this.props.ip}", ${this.props.port}), ${isUnityEditor ? 'True' : 'False'}, connect_default_device=False)
-        globals()['poco'] = poco
-
-    try:
-        h = poco.agent.hierarchy.dump()
-    except Exception as e:
-        sys.stderr.write('Error: cannot dump hierarchy from remote device. {}'.format(e.message))
-        sys.stderr.flush()
-    else:
-        print("${HierarchyBoundary}")
-        print(json.dumps(h))
-        print("${HierarchyBoundaryEnd}")
-
-    try:
-        pf = poco.agent.get_debug_profiling_data()
-        print("${ProfileDataBoundary}")
-        print(json.dumps({'dump': pf['dump'], 'dumpSerialize': pf['handleRpcRequest'] - pf['dump']}))
-        print("${ProfileDataBoundaryEnd}")
-    except Exception as e:
-        sys.stderr.write('Error: cannot get debug profiling data from remote device. {}'.format(e.message))
-        sys.stderr.flush()
-
-    try:
-        s, fmt = poco.snapshot(${width})
-    except Exception as e:
-        sys.stderr.write('Error: cannot take screenshot from remote device. {}'.format(e.message))
-        sys.stderr.flush()
-    else:
-        print("${ScreenBoundary}")
-        print("data:image/" + fmt + ";base64," + s)
-        print("${ScreenBoundaryEnd}")
-
-    try:
-        pf = poco.agent.get_debug_profiling_data()
-        print("${ProfileDataBoundary}")
-        print(json.dumps({'screenshot': pf['screenshot']}))
-        print("${ProfileDataBoundaryEnd}")
-    except Exception as e:
-        sys.stderr.write('Error: cannot get debug profiling data from remote device. {}'.format(e.message))
-        sys.stderr.flush()
-
-get_hierarchy_and_screen()
-
-# end-proc #
-`
-        this.execPy(code)
+        this.refreshScreen(width)
+        this.refreshDumper()
     }
+
     getSDKVersion() {
         let isUnityEditor = this.props.platform === 'windows' && this.props.options.isUnityEditor
         let code = `
