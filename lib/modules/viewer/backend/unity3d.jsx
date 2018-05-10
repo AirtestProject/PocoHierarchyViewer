2b1202a9d25510ede2e876c7e01e83a94b30cc84
祝啸风 <zxfn4514@corp.netease.com>
2018-05-10 18:06:27 +0800

    添加profiledata和sdkversion获取；修复传输层多条信息时后面信息无法正确读取的问题

--- lib/modules/viewer/backend/unity3d.jsx
+++ lib/modules/viewer/backend/unity3d.jsx
@@ -1,7 +1,6 @@
 
 import React from 'react'
 import autoBind from 'react-autobind'
-import rp from 'request-promise'
 import toastr from 'toastr'
 
 import {Icon} from '../../util/icon'
@@ -12,20 +11,9 @@ const RpcClient =  require('../../jsrpc/rpcclient')
 const clientSocket = require('../../jsrpc/transport')
 const Screen = require('../../poco-driver/std/screen')
 const Dumper = require('../../poco-driver/std/dumper')
+const StdPocoAgent = require('../../poco-driver/std')
 
 const Promise = window.require('bluebird')
-const {spawn} = window.require('child_process')
-
-const HierarchyBoundary = '-----H1eRarCHy-B0UNDARY-!@#$%^&&*+----'
-const HierarchyBoundaryEnd = '-----H1eRarCHy-B0UNDARY-!@#$%^&&*+----1Nd---end---'
-const ScreenBoundary = '-----sCr11n-B0UNdARY-!@#$%^&&*+----'
-const ScreenBoundaryEnd = '-----sCr11n-B0UNdARY-!@#$%^&&*+----1Nd---end---'
-const ProfileDataBoundary = '-----pr0F1Le-B0UNdARY-!@#$%^&&*+----'
-const ProfileDataBoundaryEnd = '-----pr0F1Le-B0UNdARY-!@#$%^&&*+----1Nd---end---'
-const SDKVersionBoundary = '-----sdkVERs1on-B0UNdARY-!@#$%^&&*+----'
-const SDKVersionBoundaryEnd = '-----sdkVERs1on-B0UNdARY-!@#$%^&&*+----1Nd---end---'
-
-const AppResourcePath = window.process.execPath.indexOf('build--') >= 0 ? `${window.process.resourcesPath}/app` : window.process.cwd()  // 区分一下打包环境和源码开发环境
 
 
 export class Unity3dInspectorView extends InspectorViewBase {
@@ -44,56 +32,58 @@ export class Unity3dInspectorView extends InspectorViewBase {
         this.connection = client.connect(this.props.ip, this.props.port).then(() => {
             this.screen = new Screen(client)
             this.dumper = new Dumper(client)
+            this.pocoAgent = new StdPocoAgent(client)
             this.refresh()
-            // this.getSDKVersion()
+            this.getSDKVersion()
         })
     }
 
     refreshScreen(width) {
-        this.screen.getScreen(width).then(res => {
+        return this.screen.getScreen(width).then(res => {
             var screenData = "data:image/" + res[1] + ";base64," + res[0]
             this.setState({screen: screenData})
         })
     }
 
     refreshDumper() {
-        this.dumper.dumpHierarchy().then(jsonHierarchy => {
+        return this.dumper.dumpHierarchy().then(jsonHierarchy => {
             this.setState({hierarchyTree: jsonHierarchy})
         })
     }
 
-    refresh(width) {
-        this.refreshScreen(width)
-        this.refreshDumper()
-    }
-
     getSDKVersion() {
-        let isUnityEditor = this.props.platform === 'windows' && this.props.options.isUnityEditor
-        let code = `
-def get_sdk_version():
-    # cache poco instance globally to speed up
-    poco = globals().get('poco')
-    if poco is None:
-        poco = UnityPoco(("${this.props.ip}", ${this.props.port}), ${isUnityEditor ? 'True' : 'False'}, connect_default_device=False)
-        globals()['poco'] = poco
-
-    try:
-        version = poco.agent.get_sdk_version()
-    except:
-        pass
-    else:
-        print("${SDKVersionBoundary}")
-        print(version)
-        print("${SDKVersionBoundaryEnd}")
-
-get_sdk_version()
-
-# end-proc #
-`  
-        this.execPy(code)
+        this.pocoAgent.getSdkVersion().then(sdkVersion => {
+            this.setState({sdkVersionCode: sdkVersion})
+        })
     }
+
+    refresh(width) {
+        this.refreshDumper().then(() => {
+            return this.pocoAgent.getDebugProfiling_data().then(res => {
+                this.setState({
+                    profileData: {
+                        dump: res.dump,
+                        dumpSerialize: res.handleRpcRequest - res.dump,
+                        screenshot: 0
+                    }
+                })
+            })
+        }).then(() => {
+            return this.refreshScreen(width)
+        }).then(() => {
+            return this.pocoAgent.getDebugProfiling_data().then(res => {
+                this.setState({
+                    profileData: {
+                        dump: this.state.profileData['dump'],
+                        dumpSerialize: this.state.profileData['dumpSerialize'],
+                        screenshot: res.screenshot
+                    }
+                })
+            })
+        })
+    }
+
     onDisconnect() {
-        this.pocoProc.kill()
     }
 
     renderCustomizedToolbar() {
